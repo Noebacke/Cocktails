0922379ec26c58effe2fe97476ead11318c9f384
Noé Backé <noeebacke@gmail.com>
2022-12-06 14:00:55 +0100

    updates

--- index.js
+++ index.js
@@ -1,22 +1,22 @@
 const root = document.getElementById('root');
 const button = document.createElement('button');
-const titleApi = document.createElement('h1');
-const instApi = document.createElement('div');
+const titleH1 = document.createElement('h1');
+const instDiv = document.createElement('div');
 const imgApi = document.createElement('img');
-const catApi = document.createElement('div');
-const ingreApi = document.createElement('div');
-const cocktail = document.createElement('div');
+const catDiv = document.createElement('div');
+const ingreDiv = document.createElement('div');
+const cocktailDiv = document.createElement('div');
 
 button.textContent = 'Click me';
 
 root.appendChild(button);
-root.appendChild(cocktail);
+root.appendChild(cocktailDiv);
 
-cocktail.appendChild(titleApi);
-cocktail.appendChild(instApi);
-cocktail.appendChild(imgApi);
-cocktail.appendChild(catApi);
-cocktail.appendChild(ingreApi);
+cocktailDiv.appendChild(titleH1);
+cocktailDiv.appendChild(instDiv);
+cocktailDiv.appendChild(imgApi);
+cocktailDiv.appendChild(catDiv);
+cocktailDiv.appendChild(ingreDiv);
 
 
 const fetchRandomCocktail = async () => {
@@ -25,35 +25,32 @@ const fetchRandomCocktail = async () => {
 }
 
 button.addEventListener('click', async () => {
-    for (let d = 0; d < 1; d++) {
-        const response = await fetchRandomCocktail();
-        await listIngredient();
-        console.log(response);
-        const randomDrink = response.drinks[0];
-        
-        cocktail.setAttribute('id', 'cocktail');
+    
+    const response = await fetchRandomCocktail();
+    await listIngredient();
+    console.log(response);
+    const randomDrink = response.drinks[0];
 
-        titleApi.textContent = `le nom du coktail est ${randomDrink.strDrink}`;
-        imgApi.setAttribute('src', randomDrink.strDrinkThumb);
-        instApi.textContent = randomDrink.strInstructions;
-        catApi.textContent = randomDrink.strCategory;
-        listIngredient();
-        
-    }
+    cocktailDiv.setAttribute('id', 'cocktail');
+
+    titleH1.textContent = `le nom du coktail est ${randomDrink.strDrink}`;
+    imgApi.setAttribute('src', randomDrink.strDrinkThumb);
+    instDiv.textContent = randomDrink.strInstructions;
+    catDiv.textContent = randomDrink.strCategory;
+    listIngredient();
+
 })
 
 const listIngredient = async () => {
-    ingreApi.textContent = "";
+    ingreDiv.textContent = "";
     for (let i = 0; i < 15; i++) {
         const res = await fetchRandomCocktail();
         let valueArray = 1+i;
         let strIngredient = "strIngredient"+valueArray;
         let strQuant = "strMeasure"+valueArray;
         let resDrinks = res.drinks[0];
-        if(resDrinks[strIngredient] == null){
-            
-        }else{
-            ingreApi.textContent += resDrinks[strIngredient] + " " +resDrinks[strQuant] + ""
+        if(resDrinks[strIngredient] != null){
+            ingreDiv.textContent += resDrinks[strIngredient] + " " +resDrinks[strQuant] + ""
         }
     }
 }    
