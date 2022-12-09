45114d60f0afb30e9e498f0a3c5460b9c2feba20
Noé Backé <noeebacke@gmail.com>
2022-12-09 12:15:10 +0100

    update js

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 const root = document.getElementById('root');
 const button = document.createElement('button');
-const titleH1 = document.createElement('h1');
-const instDiv = document.createElement('div');
-const imgApi = document.createElement('img');
-const catDiv = document.createElement('div');
+// const titleH1 = document.createElement('h1');
+// const instDiv = document.createElement('div');
+// const imgApi = document.createElement('img');
+// const catDiv = document.createElement('div');
 const ingreDiv = document.createElement('div');
 const cocktailDiv = document.createElement('div');
 
@@ -13,56 +13,78 @@ root.appendChild(button);
 root.appendChild(cocktailDiv);
 
 
-const fetchRandomCocktail = async () => {
-    const responseCoktail = await fetch('https://www.thecocktaildb.com/api/json/v1/1/random.php');
-    const response = await responseCoktail.json();
-    return response.drinks[0];
-
-}
-
 button.addEventListener('click', async () => {
-    const randomDrink = await fetchRandomCocktail();
+
+    let obj = new returnIngredients;
+    let obj2 = new returnElement;
+    let obj3 = new returnFetch;
+    const randomDrinks = await obj3.fetchRandomCocktail('https://www.thecocktaildb.com/api/json/v1/1/search.php?s=');
+    console.log(randomDrinks,'randomdrink');
+
+    for (let i = 0; i < 50; i++) {
+        cocktailDiv.setAttribute('id', 'cocktail');
+        const element = randomDrinks[i];
+        obj2.addElement('h1', element.strDrink, null);
+        obj2.addElement('img', null , { 'src' : element.strDrinkThumb});
+        obj2.addElement('div', element.strInstructions, null);
+        obj2.addElement('div', element.strCategory, null);
+        console.log(obj.listIngredient('https://www.thecocktaildb.com/api/json/v1/1/search.php?s='),"yo");
+        // obj2.addElement('div',)
+    }
     
-    cocktailDiv.setAttribute('id', 'cocktail');
-    addElement('h1', `le nom du coktail est ${randomDrink.strDrink}`, null);
-    addElement('img', null , { 'src' : randomDrink.strDrinkThumb});
-    addElement('div', randomDrink.strInstructions, null);
-    addElement('div', randomDrink.strCategory, null);
-
-    await listIngredient(randomDrink);
-
 })
 
-const addElement = (element, text, attrs)=>{
-
-    const el = document.createElement(element);
-
-    if (attrs) {
-
-        for (const property in attrs) {
-            console.log(attrs[property])
-            console.log(property)
-            el.setAttribute(property, attrs[property]);
+class returnElement {
+    addElement (element, text, attrs){
+        const el = document.createElement(element);
+        if (attrs) {
+            for (const property in attrs) {
+                console.log(attrs[property])
+                console.log(property)
+                el.setAttribute(property, attrs[property]);
+            }  
         }
-        
+        if (text) {
+            el.textContent = text;
+        }
+        cocktailDiv.appendChild(el);    
     }
-
-    if (text) {
-        el.textContent = text;
-    }
-
-    cocktailDiv.appendChild(el);
-        
 }
 
-const listIngredient = async (resDrinks) => {
-    ingreDiv.textContent = "";
-    for (let i = 0; i < 15; i++) {
-        let valueArray = 1+i;
-        let strIngredient = "strIngredient"+valueArray;
-        let strQuant = "strMeasure"+valueArray;
-        if(resDrinks[strIngredient] != null){
-            ingreDiv.textContent += resDrinks[strIngredient] + " " +resDrinks[strQuant] + ""
-        }
+class returnFetch {
+    async fetchRandomCocktail (link) {
+        const responseCoktail = await fetch(link);
+        const response = await responseCoktail.json();
+        return response.drinks;
     }
-}    
+}
+
+class returnIngredients {
+    async listIngredient (link) {
+        const fetchIngredients = new returnFetch;
+        const returnOfData = await fetchIngredients.fetchRandomCocktail('https://www.thecocktaildb.com/api/json/v1/1/search.php?s=');
+        ingreDiv.textContent = "";
+        for (let i = 0; i < Array.length; i++) {
+            console.log(returnOfData[Array.length],"nop");
+            if(returnOfData[Array.length].strIngredient[i] != null){
+                ingreDiv.textContent += returnOfData[Array.length].strIngredient[i] + " " +returnOfData[Array.length].strMeasure[i] + ""
+            }else{
+                console.log("ça marche pass");
+            }
+        }
+    }   
+}
+  
+
+
+
+
+
+
+// créer un nouveau bouton
+// au clic sur ce bouton, faites un appel fetch https://www.thecocktaildb.com/api/json/v1/1/search.php?s=
+// affichez les résultats dans votre DOM 
+// créez vos différentes fonctions dans une ou plusieurs classes
+// Si vous avez terminé, tentez de refactoriser votre classe en plusieurs classes
+// en utilisant le principe SRP de SOLID// https://www.freecodecamp.org/news/solid-principles-single-responsibility-principle-explained/
+
